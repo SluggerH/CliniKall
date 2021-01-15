ac96b72af7e6858acc1ff988eb2690f4b00ce97d
CaroleDEMIT <74536224+CaroleDEMIT@users.noreply.github.com>
2021-01-15 13:54:21 +0100

    Add files via upload

---
+++ AdminController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(UserRepository $userRepository,Request $request): Response
+    {
+        $users=$userRepository->findBy(
+        array(
+            'isVerified'=> 'true'
+        ),
+        array(
+            'lastname'=> 'ASC',
+            'firstname'=> 'ASC'
+        )
+        );
+
+        $search=$request->query->get('search');
+        if($search){
+            $search_users=$userRepository->search($search);
+        } else {
+            $search_users=$userRepository->findAll();
+        }
+
+        return $this->render('admin/index.html.twig', [
+            'users' => $users,
+            'search_users'=>$search_users
+        ]);
+    }
+
+    /**
+     * @Route("/admin_delete_user/{id}", name="admin_delete_user")
+     */
+    public function deleteUser(User $user) 
+    {
+          $user->getUsername();
+          $em=$this->getDoctrine()->getManager();
+          $em->remove($user);
+          $em->flush();
+        
+          $this->addFlash('success',"Le compte a bien été supprimé.");
+
+        return $this->redirectToRoute('admin'); 
+
+    }
+}
---
+++ RegistrationController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Form\RegistrationFormType;
+use App\Security\AppAuthenticator;
+use App\Security\EmailVerifier;
+use App\Service\EmailService;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+use Nzo\UrlEncryptorBundle\Encryptor\Encryptor;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+
+class RegistrationController extends AbstractController
+{
+    private $encryptor;
+
+    public function __construct(Encryptor $encryptor)
+    {
+        $this->encryptor = $encryptor;
+    }
+
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder,EmailService $emailService): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $user->setRoles(['ROLE_USER']);
+            
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $token=$this->encryptor->encrypt($user->getEmail());
+
+            // generate a signed url and email it to the user
+            $emailService->send([
+                'to'=>$user->getEmail(),
+                'subject'=>"Inscription sur ClinKall",
+                'template'=>'email/confirmation_email.email.twig',
+                'context'=>[
+                    'link' => $this->generateUrl('app_verify_email', [ 'token' => $token ], UrlGeneratorInterface::ABSOLUTE_URL)
+                ]
+            ]);
+            // do anything else you need here, like send an email
+            $this->addFlash('success',"Votre inscription est prise en compte.Merci de valider votre email.");
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/verify/email", name="app_verify_email")
+     */
+    public function verifyUserEmail(Request $request,UserRepository $userRepository,AppAuthenticator $authenticator): Response
+    {
+        $token = $request->query->get('token');
+        $email = $this->encryptor->decrypt($token);
+        $user = $userRepository->findOneBy([ 'email' => $email ]);
+        if (!$user) {
+            $this->createAccessDeniedException();
+        }
+
+        $user->setIsVerified(true);
+    
+        $entityManager=$this->getDoctrine()->getManager();
+        $entityManager->flush();
+
+        // @TODO Change the redirect on success and handle or remove the flash message in your templates
+        $this->addFlash('success', "Votre compte est bien validé.");
+
+        return $this->redirectToRoute('app_login');
+    }
+}
